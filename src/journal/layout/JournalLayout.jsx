f64441a9a2c373cfe0ef77675de04de7112bd58d
Lexxus-Cris <urielcris.97@gmail.com>
2024-07-07 23:39:27 -0600

    Maquetado del sidebar e implementado en el Layout

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,5 @@
-import { Box } from "@mui/material"
-import { Navbar } from "../components";
+import { Box, Toolbar } from "@mui/material"
+import { Navbar, Sidebar } from "../components";
 
 const drawerWidth = 240;
 
@@ -10,13 +10,14 @@ export const JournalLayout = ({ children }) => {
             <Navbar drawerWidth={drawerWidth} />
 
             {/* Sidebar */}
+            <Sidebar drawerWidth={drawerWidth} />
 
             <Box
                 component='main'
                 sx={{ flexGrow: 1, p: 3}}
             >
                 {/* Toolbar */}
-
+                <Toolbar />
                 {children}
             </Box>
 
