3af68b399810359684b82e2a7fdd7eee1924d2c1
Uriel Lessem <uriel.pineda@egas.com.mx>
2024-07-08 17:42:44 -0600

    Configuración inicial de redux y creación del store

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,15 @@ import '@fontsource/roboto/700.css';
 import './styles.css'
 import JournalApp from './JournalApp'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
