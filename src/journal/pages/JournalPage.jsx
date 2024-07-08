1b15463dc2d34df509b9d5cdeee6f5c120679c10
Lexxus-Cris <urielcris.97@gmail.com>
2024-07-07 22:59:42 -0600

    Estructura de Layout y la página principal de la sección Journal

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,10 @@
+import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
     return (
-        <div>JournalPage</div>
+        <JournalLayout>
+            <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni neque, tempora repellendus dolore tenetur, exercitationem ad obcaecati qui quas accusamus cumque eligendi amet quae debitis ab voluptatibus? Assumenda, deleniti. Cumque?</Typography>
+        </JournalLayout>
     )
 }
