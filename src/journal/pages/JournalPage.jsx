e20812ecf635958cd5ba4f05469f7d5c3a025c1a
Lexxus-Cris <urielcris.97@gmail.com>
2024-07-07 23:48:40 -0600

    Maquetado simple de NothingSelectedView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,11 @@
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
-            <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni neque, tempora repellendus dolore tenetur, exercitationem ad obcaecati qui quas accusamus cumque eligendi amet quae debitis ab voluptatibus? Assumenda, deleniti. Cumque?</Typography>
+            <NothingSelectedView />
         </JournalLayout>
     )
 }
