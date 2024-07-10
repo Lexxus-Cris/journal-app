7d455a6dd28ba721df7066b74ed076e9c5866a82
Uriel Lessem <uriel.pineda@egas.com.mx>
2024-07-10 12:44:32 -0600

    Se genera función en el custom hook useForm que realiza la validación de los inputs dependiendo de las reglas que se envien. Se implementan las nuevas caracterisitcas de useForm en Register Page

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,14 +1,43 @@
-import PropTypes from 'prop-types'
 import { AuthLayout } from '../layout/AuthLayout'
 import { Button, Grid, Link, TextField, Typography } from '@mui/material'
-import { Google } from '@mui/icons-material'
-import { Link as RouterLink } from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom';
+import { useForm } from '../../hooks';
+import { useState } from 'react';
+
+// Reglas de validación para el formulario de registro 
+const formValidations = {
+    email: [ (value) => value.includes('@'), 'El correo debe contener una @'],
+    password: [ (value) => value.length >= 6, 'El password debe tener más de seis caracteres'],
+    name: [ (value) => value.length >= 1, 'El nombre es obligatorio']
+}
 
 // Página para que el usuario se registre si no tiene una cuenta
 export const RegisterPage = () => {
+
+    const [formSubmited, setFormSubmited] = useState(false)
+
+    const {
+        formState, name, email, password, onInputChange,
+        isFormValid, nameValid, emailValid, passwordValid
+    } = useForm({
+        name: '',
+        email: '',
+        password: ''
+    }, formValidations);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFormSubmited(true)
+
+        if ( !isFormValid) return;
+
+        console.log(formState);
+    }
+
     return (
         <AuthLayout title='Registro'>
-            <form action="">
+            <Typography variant='h6'>FormValid: { isFormValid ? 'Válido' : 'Incorrecto' }</Typography>
+            <form onSubmit={handleSubmit}>
                 <Grid container >
                     <Grid item
                         xs={12}
@@ -18,9 +47,14 @@ export const RegisterPage = () => {
                     >
                         <TextField
                             label='Nombre completo'
-                            type='text'
+                            type='email'
                             placeholder='Nombre completo'
                             fullWidth
+                            name='name'
+                            value={name}
+                            onChange={onInputChange}
+                            error={ !!nameValid && formSubmited }
+                            helperText={ formSubmited && nameValid }
                         />
                     </Grid>
                     <Grid item
@@ -34,6 +68,11 @@ export const RegisterPage = () => {
                             type='email'
                             placeholder='tucorreo@gmail.com'
                             fullWidth
+                            name='email'
+                            value={email}
+                            onChange={onInputChange}
+                            error={ !!emailValid && formSubmited}
+                            helperText={ formSubmited && emailValid }
                         />
                     </Grid>
                     <Grid item
@@ -47,6 +86,11 @@ export const RegisterPage = () => {
                             type='password'
                             placeholder='contraseña'
                             fullWidth
+                            name='password'
+                            value={password}
+                            onChange={onInputChange}
+                            error={ !!passwordValid && formSubmited }
+                            helperText={ formSubmited && passwordValid }
                         />
                     </Grid>
 
@@ -58,7 +102,11 @@ export const RegisterPage = () => {
                             xs={12}
                             sm={6}
                         >
-                            <Button variant='contained' fullWidth >
+                            <Button
+                                type='submit'
+                                variant='contained'
+                                fullWidth
+                            >
                                 Crear cuenta
                             </Button>
                         </Grid>
