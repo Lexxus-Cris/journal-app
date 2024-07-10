65304366ff0417345e2ca43be6f79eca6f609a1e
Uriel Lessem <uriel.pineda@egas.com.mx>
2024-07-09 18:39:25 -0600

    Modificaciones en el estado utilizando los reducers de login y logout, se hace la petición en el archivo thunks

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,5 +1,5 @@
 import { signInWithGoogle } from "../../firebase/providers";
-import { checkingCredentials } from "./authSlice";
+import { checkingCredentials, login, logout } from "./authSlice";
 
 // Metódo asincrono para hacer la validación de login por medio de email y password
 export const checkingAuthentication = (email, password) => {
@@ -16,5 +16,9 @@ export const startGoogleSignIn = () => {
         dispatch(checkingCredentials());
 
         const result = await signInWithGoogle();
+
+        if (!result.ok) dispatch(logout(result.errorMessage));
+
+        dispatch(login(result))
     }
 }
